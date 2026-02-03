0162fcb282ee44448e2edbd575d54dd65290516f
Brett Henderson <bretth18@gmail.com>
2026-02-03 14:05:18 -0800

    Initial Commit

---
+++ seeleseek/seeleseekApp.swift
@@ -0,0 +1,17 @@
+//
+//  seeleseekApp.swift
+//  seeleseek
+//
+//  Created by Brett Henderson on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct seeleseekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
