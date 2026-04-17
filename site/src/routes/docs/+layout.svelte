b924e30778282ccd82d5416003d68e642cc8c09e
Brett Henderson <bretth18@gmail.com>
2026-04-16 17:27:42 -0700

    site: mobile layout

--- site/src/routes/docs/+layout.svelte
+++ site/src/routes/docs/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+import DocsMobileNav from '$lib/components/DocsMobileNav.svelte';
 import Sidebar from '$lib/components/Sidebar.svelte';
 
 let { data, children } = $props();
 </script>
 
+<DocsMobileNav sections={data.sections} />
+
 <div class="mx-auto flex max-w-6xl gap-10 px-5 md:px-10 py-8">
-	<Sidebar sections={data.sections} />
+	<div class="hidden lg:block">
+		<Sidebar sections={data.sections} />
+	</div>
 	<div class="min-w-0 flex-1">
 		{@render children()}
 	</div>
